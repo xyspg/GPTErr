12b81d3642f266011ca55f0c2dc0b71bc454f99f
Kang Jiaming <kangjm2005@hotmail.com>
2023-04-16 16:32:04 +0800

    fix: meta og

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -6,12 +6,12 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>GPTErr</title>
         <meta
           name="description"
           content="Generate grammar errors in your text"
         />
         <meta property="og:site_name" content="Grammar Error Generator" />
+        <meta property="og:title" content="GPTErr" />
         <meta
           property="og:image"
           content="https://gpt-err.vercel.app/og-image.png"
